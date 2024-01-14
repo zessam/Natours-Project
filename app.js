56615c6a51291fa4a0fa855cf0615bffabd70d35
Zeyad Essam <zeyadabdelhakeem1@gmail.com>
2024-01-14 04:08:21 +0200

    Init the App

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Hello from root', 
+        app: 'Natours'
+    });
+})
+
+
+app.post('', (req, res) =>{
+    res.send('You can post to this endpoint...')
+})
+
+const port = 8000
+app.listen(port, () =>{
+    console.log(`App running on port ${port}...`)
+});
+
+
