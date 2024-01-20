edea16f643146a0ba113503a43eab326d13150f7
Zeyad Essam <zeyadabdelhakeem1@gmail.com>
2024-01-20 16:53:13 +0200

    3rd party middleware morgan

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 const express = require('express');
 const fs = require('fs');
-
+const morgan = require('morgan');
 
 const app = express();
-// Middleware
+
+// 1) MIDDLEWARES
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware')
-  next()
-})
+  console.log('Hello from the middleware');
+  next();
+});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  next()
-}) 
-
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
-  console.log(req.requestTime)
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     requestAt: req.requestTime,
@@ -102,17 +103,56 @@ const deleteTour = (req, res) => {
   });
 };
 
+
+
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Not Yet Implemented'
+  })
+}
+
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Not Yet Implemented'
+  })
+}
+
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Not Yet Implemented'
+  })
+}
+
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Not Yet Implemented'
+  })
+}
+
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Not Yet Implemented'
+  })
+}
+
+
+
 //app.get('/api/v1/tours', getAllTours);
 // app.get('/api/v1/tours/:id', getTour);
 // app.post('/api/v1/tours', createTour);
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
-app
-  .route('/api/v1/tours')
-  .get(getAllTours)
-  .post(createTour);
-
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
 app
   .route('/api/v1/tours/:id')
@@ -120,6 +160,14 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
+// 4) START SERVER
 const port = 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
