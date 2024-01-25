545ab642780e8a6248be053daa5073a86575e92b
Zeyad Essam <zeyadabdelhakeem1@gmail.com>
2024-01-25 20:03:58 +0200

    enriching the tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,16 +4,52 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A Tour Must have a name'],
-    unique: true
+    unique: true,
+    trim: true
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A Tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A Tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty']
+  },
+  ratingAverage: {
     type: Number,
     default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
   },
   price: {
     type: Number,
     required: [true, 'A Tour Must have a price']
-  }
+  },
+  priceDiscount: Number,
+  Summary: {
+    type: String,
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String, 
+    required: [true, 'A tour must have a cover image']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
