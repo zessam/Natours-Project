7ba64eba4ce6bb80c8bff0650fcd5cc150a46ce6
Zeyad Essam <zeyadabdelhakeem1@gmail.com>
2024-01-29 03:06:00 +0200

    Add filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,8 +3,16 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD THE QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+    const query = Tour.find(queryObj);
 
+    // EXECUTE THE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
