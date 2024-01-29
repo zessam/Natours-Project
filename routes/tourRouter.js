0b1fd9e56a0262a5e7efff9cd826703a3f7bf682
Zeyad Essam <zeyadabdelhakeem1@gmail.com>
2024-01-29 05:38:07 +0200

    aggregation

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -5,6 +5,9 @@ const fs = require('fs');
 const router = express.Router();
 
 //router.param('id');
+router.route('/tour-stats').get(tourController.getTourStats)
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
 
 router
   .route('/top-5-cheap')
