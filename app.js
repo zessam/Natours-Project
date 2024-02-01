292f800f732a24ed91e62a64fe1f8708521d1a0b
Zeyad Essam <zeyadabdelhakeem1@gmail.com>
2024-02-01 05:22:44 +0200

    handling-unhandedled-routes

--- app.js
+++ app.js
@@ -15,9 +15,11 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
 //app.use(morgan('dev'));
 // serve static files
-// app.use(express.static(`${__dirname}/public`));
+// 
 // app.use((req, res, next) => {
 //   console.log('Hello from the middleware');
 //   next();
@@ -31,6 +33,13 @@ app.use((req, res, next) => {
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) =>{
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl}`
+  })
+})
 
 // START SERVER
 module.exports = app;
