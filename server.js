90268852ab4aa5917a6402041cb0f6e7794df6c9
Zeyad Essam <zeyadabdelhakeem1@gmail.com>
2024-02-06 09:30:34 +0200

    unhandled&&uncaught

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
@@ -41,6 +48,14 @@ mongoose
 //console.log(process.env)
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
